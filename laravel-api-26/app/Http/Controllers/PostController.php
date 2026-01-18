f3bd33b7bb44d0c15323e74b5c8de3118048b321
Sajeeb Khan <sbhossain567@gmail.com>
2026-01-18 22:33:13 +0600

    feat: add PostController with CRUD methods and update API routes

---
+++ laravel-api-26/app/Http/Controllers/PostController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return [
+            'message' => 'hello',
+            'status' => 'success',
+            'data' => [
+                'id' => 1,
+                'Title' => 'Test One',
+                'body' => 'Post body'
+            ]
+        ];
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
