80c31e3d6f91a37f4700ebc7d8be2a7163f10827
Sajeeb Khan <sbhossain567@gmail.com>
2026-01-21 22:43:51 +0600

    feat: configuration file and added CORS configuration for API.

--- laravel-api-26/routes/api.php
+++ laravel-api-26/routes/api.php
@@ -1,13 +1,8 @@
 <?php
 
-use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
-
-Route::prefix('v1')->group( function() {
-    Route::apiResource('posts', PostController::class);
 });
